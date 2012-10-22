0007ff782a2fab6711714013e21e6762ceddf2d2
Juan David Hincapié-Ramos <jhincapie@gmail.com>
2012-10-22 17:30:07 -0500

    feature tracking spike

---
+++ Src/DeviceApp/FeatureTrackingSpike/PageLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FeatureTrackingSpike
+{
+  public class PageLocation
+  {
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Theta { get; set; }
+  }
+}
