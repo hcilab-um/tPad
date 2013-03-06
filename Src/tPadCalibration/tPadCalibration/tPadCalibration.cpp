8e931503eb96957ffcdd7181ec5aec6a2acf5f2b
Sophie Roscher <sophie.roscher@gmail.com>
2013-03-06 11:45:16 -0600

    tPad Calibration with camera

--- Src/tPadCalibration/tPadCalibration/tPadCalibration.cpp
+++ Src/tPadCalibration/tPadCalibration/tPadCalibration.cpp
@@ -139,7 +139,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	cv::Mat pageImage = cv::imread("CalibrationPattern.png", CV_LOAD_IMAGE_GRAYSCALE);
 	cv::vector<cv::Point2f> cornersPage;
 	cv::findChessboardCorners(pageImage, cv::Size(8,6), cornersPage);
-
+	
 	char key;
 	std::cout << "=== Camera Calibration === \n" << std::endl;
 	std::cout << "\nPlease connect the camera to the tPad, print the calibration pattern and place it below the tPad. \nStart camera now? [y/n]" << std::endl;
@@ -151,12 +151,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (;;)
 	{
 		std::cout << "Please place the calibration pattern below the tPad and press enter when the camera captures the whole calibration pattern. \nThe pattern's edges should be aligned parallel to the device's edges." << std::endl;
-		/*if (connectCamera() == -1)
-			return -1;*/
+		if (connectCamera() == -1)
+			return -1;
 		cv::Mat cameraImage;
 		for (;;)
 		{
-			cameraImage = cv::imread("cameraImg.png", CV_LOAD_IMAGE_GRAYSCALE); //loadCameraImage();
+			cameraImage = loadCameraImage();// cv::imread("cameraImg.png", CV_LOAD_IMAGE_GRAYSCALE); /
 			cv::imshow( "Camera", cameraImage );
 
 			if ( cvWaitKey(1) == 13 )
@@ -167,12 +167,12 @@ int _tmain(int argc, _TCHAR* argv[])
 
 			if ( cvWaitKey(1) == 27 )
 			{
-				//disconnectCamera();
+				disconnectCamera();
 				return 0;
 			}
 		}
-		/*if (disconnectCamera() == -1)
-			return -1;*/
+		if (disconnectCamera() == -1)
+			return -1;
 
 		std::cout << "Warping Matrix is computed ..." << std::endl;
 		cv::Mat imgBinary;
@@ -182,44 +182,45 @@ int _tmain(int argc, _TCHAR* argv[])
 		cv::findChessboardCorners(imgBinary, cv::Size(8,6), cornersCamera);
 		//cv::drawChessboardCorners(imgBinary, cv::Size(8,6), cornersCamera, true);
 	
-		cv::Mat homography = cv::Mat( 3, 3, CV_32FC1 );
-		homography = cv::findHomography(cornersCamera, cornersPage);
+		if (cornersCamera.size() == 8*6)
+		{
+			cv::Mat homography = cv::Mat( 3, 3, CV_32FC1 );
+			homography = cv::findHomography(cornersCamera, cornersPage);
 
-		cv::FileStorage fs("homography.xml", cv::FileStorage::WRITE );
-		fs << "homography" << homography; 
-		fs.release();
+			cv::FileStorage fs("homography.xml", cv::FileStorage::WRITE );
+			fs << "homography" << homography; 
+			fs.release();
 
-		std::cout << "Warping Matrix is saved." << std::endl;
-		std::cout << "Please check whether the calibration was succesful or not. \n[Close the images and enter 'y' or 'n'.]" << std::endl;
+			std::cout << "Warping Matrix is saved." << std::endl;
+			std::cout << "Please check whether the calibration was succesful or not. \n[Close the images and enter 'y' or 'n'.]" << std::endl;
 
-		cv::warpPerspective(cameraImage, cameraImage, homography, pageImage.size());
-		cv::resize(cameraImage, cameraImage, cv::Size(cameraImage.cols * 0.5, cameraImage.rows * 0.5));
-		cv::resize(pageImage, pageImage, cv::Size(pageImage.cols * 0.5, pageImage.rows * 0.5));
-		imshow( "Result", cameraImage);	
-		imshow( "Original", pageImage);	
-		cvWaitKey(0);
-		cvDestroyAllWindows();
+			cv::warpPerspective(cameraImage, cameraImage, homography, pageImage.size());
 
-		std::cout << "Is the calibration pattern aligned correctly? [y/n]." << std::endl;
-		std::cin >> key;
+			cv::resize(cameraImage, imgBinary, cv::Size(cameraImage.cols * 0.5, cameraImage.rows * 0.5));			
+			imshow( "Result", imgBinary);
+			cv::resize(pageImage, pageImage, cv::Size(pageImage.cols * 0.5, pageImage.rows * 0.5));
+			imshow( "Original", pageImage);	
+			cvWaitKey(0);
+			cvDestroyAllWindows();
 
-		if (key == 'y')
-			break;
+			std::cout << "Is the calibration pattern aligned correctly? [y/n]." << std::endl;
+			std::cin >> key;
+
+			if (key == 'y')
+				break;
+			else 
+			{
+				std::cout << "Press Escape to close the program. \n\n" <<std::endl;
+				cvDestroyAllWindows();
+				continue;
+			}
+		}
 		else 
 		{
+			std::cout << "\nComputation of warpingwhich squarecsdcdsvc matrix failed! Please try again.\n" <<std::endl;
 			std::cout << "Press Escape to close the program. \n\n" <<std::endl;
-			cvDestroyAllWindows();
 			continue;
-		}
-
-		/*cv::Mat testImage = cv::imread("test.png", CV_LOAD_IMAGE_GRAYSCALE);
-	
-		cv::Mat homography2 = cv::Mat( 3, 3, CV_32FC1 );
-		cv::FileStorage fw("homography.xml", cv::FileStorage::READ );
-		fw["homography"] >> homography2; 
-		fw.release();
-		imshow( "Match", testImage );
-		*/
+		}		
 	}
 	return 0;
 }
