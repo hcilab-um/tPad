d12473b62ba52d509a2124ccf77bad8e601ae6dd
Juan David Hincapié-Ramos <jhincapie@gmail.com>
2013-06-14 17:08:36 -0500

    Simple counter-based approach to determine the side

--- Src/Arduino/tPad/tPad.ino
+++ Src/Arduino/tPad/tPad.ino
@@ -61,6 +61,18 @@ void loop(){
   y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
   z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
 
+  //Calculates the side that's on top. 1: front up | -1: reverse up
+  int orientation = -1;
+  int count = 0;
+  if(x >= 270 || x <= 90)
+    count++;
+  if(y >= 270 || y <= 90)
+    count++;
+  if(z >= 90 && z <= 360)
+    count ++;
+  if(count >= 2)
+    orientation = 1;
+  
   //read the digital values for the staking codes
   int stackCode0 = digitalRead(stackPin0);
   int stackCode1 = digitalRead(stackPin1);
@@ -69,7 +81,12 @@ void loop(){
 
   //Output the caculations
   Serial.print("{");
-  Serial.print("\"Orientation\": { \"X\": ");
+  Serial.print("\"Orientation\": { \"Side\": ");
+  if(orientation == 1)
+    Serial.print("\"Front\", ");
+  else
+    Serial.print("\"Reverse\", ");
+  Serial.print("\"X\": ");
   Serial.print(x);
   Serial.print(", \"Y\": ");
   Serial.print(y);
