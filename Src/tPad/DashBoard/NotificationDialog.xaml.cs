dc24f653d595e436c798a2e2461e73ec0f952c51
Juan David Hincapié-Ramos <jhincapie@gmail.com>
2013-07-02 23:59:22 -0500

    More for Notifications and Flipping

---
+++ Src/tPad/DashBoard/NotificationDialog.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace UofM.HCI.tPad.App.Dashboard
+{
+  /// <summary>
+  /// Interaction logic for NotificationDialog.xaml
+  /// </summary>
+  public partial class NotificationDialog : UserControl, ITPadApp, INotifyPropertyChanged
+  {
+
+    public event EventHandler Closed;
+    public event BoolEventHandler IsTopApp;
+    public event RequestUserFocus RequestFocus;
+
+    public TPadCore Core { get; set; }
+    public ITPadAppContainer Container { get; set; }
+    public ITPadAppController Controller { get; set; }
+    public Dictionary<String, String> Context { get { return null; } }
+
+    private String message = String.Empty;
+    public String Message 
+    {
+      get { return message; }
+      set 
+      {
+        message = value;
+        OnPropertyChanged("Message");
+      }
+    }
+
+    public NotificationDialog()
+    {
+      InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged(String name)
+    {
+      if (PropertyChanged != null)
+        PropertyChanged(this, new PropertyChangedEventArgs(name));
+    }
+
+    public void LoadInitContext(Dictionary<string, string> init) {}
+
+    public void Close()
+    {
+      if (Closed != null)
+        Closed(this, null);
+    }
+  }
+}
---
+++ Src/tPad/tPadLib/Network/FakeWhatUpMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jayrock.Json.Conversion;
+using Ubicomp.Utils.NET.MTF;
+
+namespace UofM.HCI.tPad.Network
+{
+  public class FakeWhatUpMessage : ITransportMessageContent, IJsonExportable
+  {
+
+    public void Export(ExportContext context, Jayrock.Json.JsonWriter writer)
+    {
+      //"from":"Device-1","message":"JUAN"
+      writer.WriteStartObject();
+      writer.WriteMember("from");
+      context.Export("Simulator", writer);
+      writer.WriteMember("message");
+      context.Export("Hello World!", writer);
+      writer.WriteEndObject();
+    }
+
+    public int MessageID
+    {
+      get { throw new NotImplementedException(); }
+    }
+  }
+}
