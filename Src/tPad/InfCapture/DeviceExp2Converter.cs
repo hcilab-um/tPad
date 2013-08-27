2dc6b4f89b8707a3b097cc26526c5766ed917780
Sophie Roscher <sophie.roscher@gmail.com>
2013-08-27 11:01:43 -0500

    Small changes during trial

--- Src/tPad/InfCapture/DeviceExp2Converter.cs
+++ Src/tPad/InfCapture/DeviceExp2Converter.cs
@@ -15,7 +15,7 @@ namespace UofM.HCI.tPad.App.InfCapture
       Device actualD = (Device)value;
       if (actualD == targetD)
         return Visibility.Visible;
-      return Visibility.Visible;
+      return Visibility.Collapsed;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
